ec3934e0c2248318c86295e5e467d9d7a92c59d5
wly <1355498705@qq.com>
2021-09-27 15:39:06 +0800

    创建一个简单的服务器

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,21 @@
 import * as http from 'http'
 
-const serve = http.createServer();
+const serve = http.createServer()
+
+serve.on('request', (request,response)=> {
+  // console.log('request.headers',request.headers);
+  // console.log('request.method',request.method);
+  const chunkArr = []
+  request.on('data', (chunk) => {
+    chunkArr.push(chunk)
+  })
+
+  request.on('end', () => {
+    const data = Buffer.concat(chunkArr).toString()
+    // console.log(Buffer.concat(chunkArr).toString());
+    response.statusCode = 200
+    response.end(data)
+  })
+})
+
+serve.listen(8000)
