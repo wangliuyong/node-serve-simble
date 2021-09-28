bdd85e2186380c98b120b8b0cd76f134069024f1
wly <1355498705@qq.com>
2021-09-28 13:33:41 +0800

    增加html实例

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ which [包名] 返回包的路径
 # curl 测试接口
 
 `curl -v -d "name=wang" http://localhost:8000`
+
+# ts-node-dev （文件更新时自动重启，让 node 支持 typescript，尚蝉环境慎用）
+
+```javascript
+npm i -g -D ts-node-dev
+```
--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,55 @@
 import * as http from 'http'
+import * as path from 'path'
+import * as fs from 'fs'
 
 const serve = http.createServer()
 
 serve.on('request', (request, response) => {
-  // console.log('request.headers',request.headers);
-  // console.log('request.method',request.method);
-  const chunkArr = []
-  request.on('data', (chunk) => {
-    chunkArr.push(chunk)
-  })
 
-  request.on('end', () => {
-    const data = Buffer.concat(chunkArr).toString()
-    // console.log(Buffer.concat(chunkArr).toString());
-    response.statusCode = 200
-    response.end(data)
-  })
+  const { url, method, headers } = request
+  const publicPath = path
+  console.log(url);
+
+  switch (url) {
+    case '/index.html':
+      fs.readFile(path.resolve(__dirname, 'public/index.html'), (error, data) => {
+        if (error) throw error
+        response.setHeader('content-type', 'text/html; charset=utf-8')
+        response.end(data.toString())
+      })
+      break;
+    case '/style.css':
+      fs.readFile(path.resolve(__dirname, 'public/style.css'), (error, data) => {
+        if (error) throw error
+        response.setHeader('content-type', 'text/css; charset=utf-8')
+        response.end(data.toString())
+      })
+      break;
+    case '/main.js':
+      fs.readFile(path.resolve(__dirname, 'public/main.js'), (error, data) => {
+        if (error) throw error
+        response.setHeader('content-type', 'application/javascript; charset=utf-8')
+        response.end(data.toString())
+      })
+      break;
+    default:
+      response.end()
+      break;
+  }
+
+  // const chunkArr = []
+  // request.on('data', (chunk) => {
+  //   chunkArr.push(chunk)
+  // })
+
+  // request.on('end', () => {
+  //   const data = Buffer.concat(chunkArr).toString()
+  //   // console.log(Buffer.concat(chunkArr).toString());
+  //   response.statusCode = 200
+  //   response.setHeader('name', 'wang')
+  //   // response.write(data)
+  //   response.end(data)
+  // })
 })
 
 serve.listen(8000)
